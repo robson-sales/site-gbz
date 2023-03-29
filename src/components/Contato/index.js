f4fb0733725db8ae2b12f71ae5ab707bd1e9ab69
robson-sales <eng.robs@gmail.com>
2023-03-29 16:33:59 -0300

    Ajuste de layout

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -14,90 +14,88 @@ import './style.css';
 export default function Contato() {
   return (
     <>
-      <div className="contact">
-        <div className="contact-content">
-          <div className="contact-content-img">
-            <CarrosselContato />
+      <div className="contact-container">
+        <div className="contact-img">
+          <CarrosselContato />
+        </div>
+        <div className="contact-txt">
+          <h2>Contato</h2>
+          <p>
+            <strong>
+              Descubra como podemos ajudar a potencializar seu negócio.
+            </strong>
+          </p>
+          <div className="contact-social">
+            <p>Nossas redes sociais:</p>
+            <a
+              href="https://www.linkedin.com/company/galvanoplastiabarueri/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsLinkedin />
+            </a>
+            <a
+              href="https://www.instagram.com/gbzincagem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/gbzincagem"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFacebook />
+            </a>
           </div>
-          <div className="contact-content-txt">
-            <h2>Contato</h2>
-            <p>
-              <strong>
-                Descubra como podemos ajudar a potencializar seu negócio.
-              </strong>
-            </p>
-            <div className="contact-social">
-              <p>Nossas redes sociais:</p>
-              <a
-                href="https://www.linkedin.com/company/galvanoplastiabarueri/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsLinkedin />
-              </a>
-              <a
-                href="https://www.instagram.com/gbzincagem/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsInstagram />
-              </a>
-              <a
-                href="https://www.facebook.com/gbzincagem"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsFacebook />
-              </a>
+          <div className="contact-data">
+            <div className="contact-item">
+              <div className="contact-ico">
+                <BsTelephoneFill />
+              </div>
+              <div className="contact-info">(11) 3199-3787 / 3199-3788</div>
             </div>
-            <div className="contact-data">
-              <div className="contact-item">
-                <div className="contact-ico">
-                  <BsTelephoneFill />
-                </div>
-                <div className="contact-info">(11) 3199-3787 / 3199-3788</div>
+            <div className="contact-item">
+              <div className="contact-ico">
+                <BsWhatsapp style={{ color: 'green' }} />
               </div>
-              <div className="contact-item">
-                <div className="contact-ico">
-                  <BsWhatsapp style={{ color: 'green' }} />
-                </div>
-                <div className="contact-info">
-                  <a
-                    href="https://api.whatsapp.com/send?phone=5511972065931"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    (11) 97206-5931
-                  </a>
-                </div>
+              <div className="contact-info">
+                <a
+                  href="https://api.whatsapp.com/send?phone=5511972065931"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  (11) 97206-5931
+                </a>
               </div>
-              <div className="contact-item">
-                <div className="contact-ico">
-                  <BsMailbox />
-                </div>
-                <div className="contact-info">
-                  <a href="mailto:contato@gb.ind.br">contato@gb.ind.br</a>
-                  <br />
-                  <a href="mailto:comercial@gb.ind.br">comercial@gb.ind.br</a>
-                </div>
+            </div>
+            <div className="contact-item">
+              <div className="contact-ico">
+                <BsMailbox />
               </div>
-              <div className="contact-item">
-                <div className="contact-ico">
-                  <BsFillPinMapFill />
-                </div>
-                <div className="contact-info">
-                  Rua João Santana Leite, 100 - Campo da Vila
-                  <br />
-                  Santana de Parnaíba - SP - CEP: 06501-238
-                </div>
+              <div className="contact-info">
+                <a href="mailto:contato@gb.ind.br">contato@gb.ind.br</a>
+                <br />
+                <a href="mailto:comercial@gb.ind.br">comercial@gb.ind.br</a>
               </div>
-              <div className="contact-map">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14642.27110747452!2d-46.917929!3d-23.4399757!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf0397dc265b5b%3A0xe75bd48052406042!2sGB%20Zincagem%20LTDA!5e0!3m2!1spt-BR!2sbr!4v1679577843744!5m2!1spt-BR!2sbr"
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+            </div>
+            <div className="contact-item">
+              <div className="contact-ico">
+                <BsFillPinMapFill />
               </div>
+              <div className="contact-info">
+                Rua João Santana Leite, 100 - Campo da Vila
+                <br />
+                Santana de Parnaíba - SP - CEP: 06501-238
+              </div>
+            </div>
+            <div className="contact-map">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14642.27110747452!2d-46.917929!3d-23.4399757!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf0397dc265b5b%3A0xe75bd48052406042!2sGB%20Zincagem%20LTDA!5e0!3m2!1spt-BR!2sbr!4v1679577843744!5m2!1spt-BR!2sbr"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
--- src/components/QuemSomos/index.js
+++ src/components/QuemSomos/index.js
@@ -1,46 +1,48 @@
+import fachada from '../../assets/images/home-banner.jpg';
+
 import './style.css';
 
 export default function Contato() {
   return (
     <>
-      <div className="qs">
-        <div className="qs-content">
-          <div className="qs-content-img"></div>
-          <div className="qs-content-txt">
-            <h2>Nossa história</h2>
-            <h4>Proteção e qualidade para o seu metal desde 1986</h4>
-            <p>
-              Com muitos anos de mercado atuante em galvanoplastia, sempre
-              aprimorando nossos banhos e tecnologias de controles. Somos
-              referência em tratamentos protetivos. Atendemos aos segmentos:
-            </p>
-            <ul>
-              <li>Eólico (onshore e offshore)</li>
-              <li>Solar</li>
-              <li>Aéreo</li>
-              <li>Óleo e gás</li>
-              <li>Construção civil</li>
-              <li>Fixadores</li>
-              <li>Linha branca</li>
-              <li>Eletro-nuclear</li>
-              <li>Petrolífera</li>
-              <li>Telecomunicações</li>
-            </ul>
-            <p>
-              Homologada pelos principais players de mercado, trabalhamos com os
-              melhores fornecedores em matérias-primas, em processos e
-              tecnologias em equipamentos. Possuímos um contingente altamente
-              treinado, controle laboratorial (próprio), com as certificações e
-              licenças de órgãos públicos e sistema de gestão de qualidade
-              certificado com base na norma NBR ISO 9001.
-            </p>
-            <p>
-              Possuímos todos os certificados e licenças requeridas para o total
-              funcionamento dentro de todos os cuidados com o meio ambiente e
-              controles governamentais. Nossa central de tratamento de efluentes
-              conta com total estrutura homologada pelos padrões da Cetesb.
-            </p>
-          </div>
+      <div className="qs-container">
+        <div className="qs-img">
+          <img src={fachada} alt="Fachada da GB Zincagem" />
+        </div>
+        <div className="qs-txt">
+          <h2>Nossa história</h2>
+          <h4>Proteção e qualidade para o seu metal desde 1986</h4>
+          <p>
+            Com muitos anos de mercado atuante em galvanoplastia, sempre
+            aprimorando nossos banhos e tecnologias de controles. Somos
+            referência em tratamentos protetivos. Atendemos aos segmentos:
+          </p>
+          <ul>
+            <li>Eólico (onshore e offshore)</li>
+            <li>Solar</li>
+            <li>Aéreo</li>
+            <li>Óleo e gás</li>
+            <li>Construção civil</li>
+            <li>Fixadores</li>
+            <li>Linha branca</li>
+            <li>Eletro-nuclear</li>
+            <li>Petrolífera</li>
+            <li>Telecomunicações</li>
+          </ul>
+          <p>
+            Homologada pelos principais players de mercado, trabalhamos com os
+            melhores fornecedores em matérias-primas, em processos e tecnologias
+            em equipamentos. Possuímos um contingente altamente treinado,
+            controle laboratorial (próprio), com as certificações e licenças de
+            órgãos públicos e sistema de gestão de qualidade certificado com
+            base na norma NBR ISO 9001.
+          </p>
+          <p>
+            Possuímos todos os certificados e licenças requeridas para o total
+            funcionamento dentro de todos os cuidados com o meio ambiente e
+            controles governamentais. Nossa central de tratamento de efluentes
+            conta com total estrutura homologada pelos padrões da Cetesb.
+          </p>
         </div>
       </div>
     </>
