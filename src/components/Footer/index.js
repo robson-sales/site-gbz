e52405123be3d750a4a0c781d0e0346eb7048d84
Robson Sales <eng.robs@gmail.com>
2024-02-22 11:52:35 -0300

    Ajuste da home e inclusão de CTA

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import './style.css';
 
 export default function Footer() {
+  const anoAtual = new Date().getFullYear();
   return (
     <div className="footer">
       <p>
         Desenvolvido com ❤️ por <a href="https://www.beeks.com.br">beeks</a> | ©
-        2023 Todos os direitos reservados
+        {anoAtual} Todos os direitos reservados
       </p>
     </div>
   );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { BsWhatsapp } from 'react-icons/bs';
 import './style.css';
 
 export default function Home() {
@@ -11,6 +12,21 @@ export default function Home() {
     <div className="banner">
       <div className="text-over-left">
         <h1>GB Zincagem</h1>
+        <h2>
+          Especialista em tratamento protetivo <br />
+          em metais, com pontualidade e<br /> preços competitivos.
+        </h2>
+        <br />
+        <a
+          href="https://api.whatsapp.com/send?phone=5511972065931"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Link para acessar nosso WhatsApp de contato"
+        >
+          <button type="submit" className="button">
+            <BsWhatsapp color="white" fontSize="12px" /> Solicite um orçamento{' '}
+          </button>
+        </a>
       </div>
     </div>
   );
